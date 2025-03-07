f7e560337c7840c1dfdee164aa4dd0ed4202457f
Puranmishra09 <109064029+Puranmishra09@users.noreply.github.com>
2025-03-07 17:56:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,14 @@
-variable "region" {
+variable "aws_region" {
   description = "AWS region"
   default     = "us-east-1"
 }
 
-variable "vpc_id" {
-  description = "VPC ID where NLB and Lambda will be deployed"
-}
-
-variable "subnet_ids" {
-  description = "List of Subnet IDs for the Load Balancer"
-  type        = list(string)
-}
-
-variable "lambda_s3_bucket" {
-  description = "S3 bucket storing Lambda zip file"
-}
-
-variable "lambda_s3_key" {
-  description = "S3 object key for Lambda zip file"
-}
-
 variable "domain_name" {
-  description = "Custom domain for API Gateway"
+  description = "The custom domain for API Gateway"
+  default     = "puran.com"
 }
 
+variable "lambda_function_name" {
+  description = "The Lambda function name"
+  default     = "puran_function"
+}
